67677373cb981f276abad96179663e8941ccf525
Leonardo Cserny <leo.cserny@gmail.com>
2023-02-15 20:30:31 +0200

    minor refactor

--- src/main/java/net/cserny/videosmover/QBitTorrentService.java
+++ src/main/java/net/cserny/videosmover/QBitTorrentService.java
@@ -1,6 +1,7 @@
 package net.cserny.videosmover;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.Dependent;
 import java.util.List;
@@ -8,12 +9,15 @@ import java.util.List;
 @Dependent
 public class QBitTorrentService implements TorrentService {
 
+    private static final Logger LOGGER = Logger.getLogger(QBitTorrentService.class);
+
     @RestClient
     QBitTorrentV2ApiClient client;
 
     @Override
     public void delete(String hash, boolean deleteFiles) {
         client.delete(hash, deleteFiles);
+        LOGGER.info("Torrent deleted with hash " + hash);
     }
 
     @Override
